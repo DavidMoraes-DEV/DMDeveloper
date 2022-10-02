07eb4eaa1ce1f4691f2c5ad71791efca67900b88
DavidMoraes-DEV <investtraderrs@gmail.com>
2022-10-01 21:03:49 -0300

    Basic Styling Home Page

--- frontweb/src/pages/Home/index.tsx
+++ frontweb/src/pages/Home/index.tsx
@@ -1,10 +1,27 @@
+import ImageCard from 'assets/images/home-page-card.png';
+import { NavLink } from 'react-router-dom';
 import './styles.css';
 
 const Home = () => {
   return (
-    <div className='home-container bg'>
-      <h1>Home Page</h1>
-      <h3>Subtitulo</h3>
+    <div className="bg home-container">
+      <div className="home-card-container">
+        <h1>Bem Vindo, ao meu Portfólio!</h1>
+        <div className="home-card-container-image">
+          <img src={ImageCard} alt="Home Page Card" />
+        </div>
+        <div className='home-card-buttons-container'>
+          <NavLink to='/projects'>
+            <button className='base-button home-card-button'>PROJETOS</button>
+          </NavLink>
+          <NavLink to='/resume'>
+            <button className='base-button home-card-button'>CURRÍCULO</button>
+          </NavLink>
+        </div>
+      </div>
+      <div className="home-image-container">
+        <h1>Minha Foto</h1>
+      </div>
     </div>
   );
 };
