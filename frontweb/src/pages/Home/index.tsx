719b7e88e4de13962a7102412c78b61ac319cc59
DavidMoraes-DEV <investtraderrs@gmail.com>
2022-12-21 15:10:50 -0300

    Update HomePage

--- frontweb/src/pages/Home/index.tsx
+++ frontweb/src/pages/Home/index.tsx
@@ -14,9 +14,9 @@ const Home = () => {
             <h2>David Moraes de Oliveira</h2>
             <h3>Desenvolvedor Web</h3>
             <p>
-              Estudante de Engenharia de Software com foco no desenvolvimento web fullstack, utilizando tecnologias como: Java - Spring Boot - TypeScript - React. Atualmente me dedico em período integral aos estudos.
-              <br /><br />Diariamente estou desenvolvendo projetos pessoais e me empenhando para aperfeiçoar os meus conhecimentos.
-              Estou sempre buscando novos conhecimentos que agreguem valor ao meu perfil de desenvolvedor. <br />
+              Estudante de Engenharia de Software com foco no desenvolvimento web fullstack, utilizando tecnologias como: Java - Spring Boot - TypeScript - React. Atualmente me dedico em período integral aos estudos,
+              diariamente estou desenvolvendo projetos pessoais, me empenhando para aperfeiçoar os meus conhecimentos e
+              buscando sempre novos conhecimentos que agreguem valor ao meu perfil de desenvolvedor. <br />
             </p>
 
             <aside className="home-apresentation-buttons-container">
