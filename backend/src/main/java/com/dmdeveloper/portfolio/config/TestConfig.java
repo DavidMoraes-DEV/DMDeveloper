36f65d17177582cd512712432a1163636b8bc6cb
DavidMoraes-DEV <investtraderrs@gmail.com>
2023-05-18 23:26:19 -0300

    Upgrade TestConfig

--- backend/src/main/java/com/dmdeveloper/portfolio/config/TestConfig.java
+++ backend/src/main/java/com/dmdeveloper/portfolio/config/TestConfig.java
@@ -73,7 +73,7 @@ public class TestConfig implements CommandLineRunner {
 		user2.getRoles().addAll(Arrays.asList(role1, role2));
 		userRepository.saveAll(Arrays.asList(user1, user2));
 		
-		Resume resume = new Resume(null, "https://raw.githubusercontent.com/DavidMoraes-DEV/DMDeveloper/master/frontweb/src/assets/files/curriculum-david.pdf");
+		Resume resume = new Resume(null, "https://github.com/DavidMoraes-DEV/ProjectsAssets/raw/main/files/curriculum-david.pdf");
 		resumeRepository.saveAll(Arrays.asList(resume));
 		
 		PersonalInfo personalinfo = new PersonalInfo(null, "David Moraes de Oliveira", Instant.parse("1997-06-20T15:15:00Z"), "Masculino", "44 99158-5419", "DavidMoraes-DEV@outlook.com", "Rua Lucio Tozini, 11", "86610-000", "Jaguapitã", "PR", "AB", resume);
@@ -84,37 +84,37 @@ public class TestConfig implements CommandLineRunner {
 		Category catg3 = new Category(null, "Backend");
 		Category catg4 = new Category(null, "Frontweb");
 		
-		Skill skill1 = new Skill(null, "h2", "https://raw.githubusercontent.com/DavidMoraes-DEV/DMDeveloper/master/frontweb/src/assets/images/logos-technologies/tec-tools/logo-h2.png", resume);
-		Skill skill2 = new Skill(null, "mysql-workbench", "https://raw.githubusercontent.com/DavidMoraes-DEV/DMDeveloper/master/frontweb/src/assets/images/logos-technologies/tec-tools/logo-mysql-workbench.png", resume);
-		Skill skill3 = new Skill(null, "postman", "https://raw.githubusercontent.com/DavidMoraes-DEV/DMDeveloper/master/frontweb/src/assets/images/logos-technologies/tec-tools/logo-postman.png", resume);
+		Skill skill1 = new Skill(null, "h2", "https://raw.githubusercontent.com/DavidMoraes-DEV/ProjectsAssets/main/dmdeveloper/skills-logo/tec-tools/logo-h2.png", resume);
+		Skill skill2 = new Skill(null, "mysql-workbench", "https://raw.githubusercontent.com/DavidMoraes-DEV/ProjectsAssets/main/dmdeveloper/skills-logo/tec-tools/logo-mysql-workbench.png", resume);
+		Skill skill3 = new Skill(null, "postman", "https://raw.githubusercontent.com/DavidMoraes-DEV/ProjectsAssets/main/dmdeveloper/skills-logo/tec-tools/logo-postman.png", resume);
 		
-		Skill skill4 = new Skill(null, "spring-tools", "https://raw.githubusercontent.com/DavidMoraes-DEV/DMDeveloper/master/frontweb/src/assets/images/logos-technologies/tec-ides/logo-spring-tools.png", resume);
-		Skill skill5 = new Skill(null, "visual-studio-code", "https://raw.githubusercontent.com/DavidMoraes-DEV/DMDeveloper/master/frontweb/src/assets/images/logos-technologies/tec-ides/logo-visual-studio-code.png", resume);
-		Skill skill6 = new Skill(null, "visual-studio", "https://raw.githubusercontent.com/DavidMoraes-DEV/DMDeveloper/master/frontweb/src/assets/images/logos-technologies/tec-ides/logo-visual-studio.png", resume);
+		Skill skill4 = new Skill(null, "spring-tools", "https://raw.githubusercontent.com/DavidMoraes-DEV/ProjectsAssets/main/dmdeveloper/skills-logo/tec-ides/logo-spring-tools.png", resume);
+		Skill skill5 = new Skill(null, "visual-studio-code", "https://raw.githubusercontent.com/DavidMoraes-DEV/ProjectsAssets/main/dmdeveloper/skills-logo/tec-ides/logo-visual-studio-code.png", resume);
+		Skill skill6 = new Skill(null, "visual-studio", "https://raw.githubusercontent.com/DavidMoraes-DEV/ProjectsAssets/main/dmdeveloper/skills-logo/tec-ides/logo-visual-studio.png", resume);
 		
-		Skill skill7 = new Skill(null, "java", "https://raw.githubusercontent.com/DavidMoraes-DEV/DMDeveloper/master/frontweb/src/assets/images/logos-technologies/tec-backend/logo-java.png", resume);
-		Skill skill8 = new Skill(null, "jpa-hibernate", "https://raw.githubusercontent.com/DavidMoraes-DEV/DMDeveloper/master/frontweb/src/assets/images/logos-technologies/tec-backend/logo-jpa-hibernate.png", resume);
-		Skill skill9 = new Skill(null, "junit5", "https://raw.githubusercontent.com/DavidMoraes-DEV/DMDeveloper/master/frontweb/src/assets/images/logos-technologies/tec-backend/logo-junit-5.png", resume);
-		Skill skill10 = new Skill(null, "jwt", "https://raw.githubusercontent.com/DavidMoraes-DEV/DMDeveloper/master/frontweb/src/assets/images/logos-technologies/tec-backend/logo-jwt.png", resume);
-		Skill skill11 = new Skill(null, "maven", "https://raw.githubusercontent.com/DavidMoraes-DEV/DMDeveloper/master/frontweb/src/assets/images/logos-technologies/tec-backend/logo-maven.png", resume);
-		Skill skill12 = new Skill(null, "mockito", "https://raw.githubusercontent.com/DavidMoraes-DEV/DMDeveloper/master/frontweb/src/assets/images/logos-technologies/tec-backend/logo-mockito.png", resume);
-		Skill skill13 = new Skill(null, "oauth2", "https://raw.githubusercontent.com/DavidMoraes-DEV/DMDeveloper/master/frontweb/src/assets/images/logos-technologies/tec-backend/logo-oauth-2.png", resume);
-		Skill skill14 = new Skill(null, "spring-boot", "https://raw.githubusercontent.com/DavidMoraes-DEV/DMDeveloper/master/frontweb/src/assets/images/logos-technologies/tec-backend/logo-spring-boot.png", resume);
+		Skill skill7 = new Skill(null, "java", "https://raw.githubusercontent.com/DavidMoraes-DEV/ProjectsAssets/main/dmdeveloper/skills-logo/tec-backend/logo-java.png", resume);
+		Skill skill8 = new Skill(null, "jpa-hibernate", "https://raw.githubusercontent.com/DavidMoraes-DEV/ProjectsAssets/main/dmdeveloper/skills-logo/tec-backend/logo-jpa-hibernate.png", resume);
+		Skill skill9 = new Skill(null, "junit5", "https://raw.githubusercontent.com/DavidMoraes-DEV/ProjectsAssets/main/dmdeveloper/skills-logo/tec-backend/logo-junit-5.png", resume);
+		Skill skill10 = new Skill(null, "jwt", "https://raw.githubusercontent.com/DavidMoraes-DEV/ProjectsAssets/main/dmdeveloper/skills-logo/tec-backend/logo-jwt.png", resume);
+		Skill skill11 = new Skill(null, "maven", "https://raw.githubusercontent.com/DavidMoraes-DEV/ProjectsAssets/main/dmdeveloper/skills-logo/tec-backend/logo-maven.png", resume);
+		Skill skill12 = new Skill(null, "mockito", "https://raw.githubusercontent.com/DavidMoraes-DEV/ProjectsAssets/main/dmdeveloper/skills-logo/tec-backend/logo-mockito.png", resume);
+		Skill skill13 = new Skill(null, "oauth2", "https://raw.githubusercontent.com/DavidMoraes-DEV/ProjectsAssets/main/dmdeveloper/skills-logo/tec-backend/logo-oauth-2.png", resume);
+		Skill skill14 = new Skill(null, "spring-boot", "https://raw.githubusercontent.com/DavidMoraes-DEV/ProjectsAssets/main/dmdeveloper/skills-logo/tec-backend/logo-spring-boot.png", resume);
 		
-		Skill skill15 = new Skill(null, "apex-charts", "https://raw.githubusercontent.com/DavidMoraes-DEV/DMDeveloper/master/frontweb/src/assets/images/logos-technologies/tec-frontweb/logo-apex-charts.png", resume);
-		Skill skill16 = new Skill(null, "axios", "https://raw.githubusercontent.com/DavidMoraes-DEV/DMDeveloper/master/frontweb/src/assets/images/logos-technologies/tec-frontweb/logo-axios.png", resume);
-		Skill skill17 = new Skill(null, "bootstrap", "https://raw.githubusercontent.com/DavidMoraes-DEV/DMDeveloper/master/frontweb/src/assets/images/logos-technologies/tec-frontweb/logo-bootstrap.png", resume);
-		Skill skill18 = new Skill(null, "css", "https://raw.githubusercontent.com/DavidMoraes-DEV/DMDeveloper/master/frontweb/src/assets/images/logos-technologies/tec-frontweb/logo-css.png", resume);
-		Skill skill19 = new Skill(null, "html", "https://raw.githubusercontent.com/DavidMoraes-DEV/DMDeveloper/master/frontweb/src/assets/images/logos-technologies/tec-frontweb/logo-html.png", resume);
-		Skill skill20 = new Skill(null, "jest", "https://raw.githubusercontent.com/DavidMoraes-DEV/DMDeveloper/master/frontweb/src/assets/images/logos-technologies/tec-frontweb/logo-jest.png", resume);
-		Skill skill21 = new Skill(null, "js", "https://raw.githubusercontent.com/DavidMoraes-DEV/DMDeveloper/master/frontweb/src/assets/images/logos-technologies/tec-frontweb/logo-js.png", resume);
-		Skill skill22 = new Skill(null, "netlify", "https://raw.githubusercontent.com/DavidMoraes-DEV/DMDeveloper/master/frontweb/src/assets/images/logos-technologies/tec-frontweb/logo-netlify.png", resume);
-		Skill skill23 = new Skill(null, "nodejs", "https://raw.githubusercontent.com/DavidMoraes-DEV/DMDeveloper/master/frontweb/src/assets/images/logos-technologies/tec-frontweb/logo-nodejs.png", resume);
-		Skill skill24 = new Skill(null, "react-hook-form", "https://raw.githubusercontent.com/DavidMoraes-DEV/DMDeveloper/master/frontweb/src/assets/images/logos-technologies/tec-frontweb/logo-react-hook-form.png", resume);
-		Skill skill25 = new Skill(null, "react-hook", "https://raw.githubusercontent.com/DavidMoraes-DEV/DMDeveloper/master/frontweb/src/assets/images/logos-technologies/tec-frontweb/logo-react-hook.png", resume);
-		Skill skill26 = new Skill(null, "react-router", "https://raw.githubusercontent.com/DavidMoraes-DEV/DMDeveloper/master/frontweb/src/assets/images/logos-technologies/tec-frontweb/logo-react-router.png", resume);
-		Skill skill27 = new Skill(null, "react", "https://raw.githubusercontent.com/DavidMoraes-DEV/DMDeveloper/master/frontweb/src/assets/images/logos-technologies/tec-frontweb/logo-react.png", resume);
-		Skill skill28 = new Skill(null, "typescript", "https://raw.githubusercontent.com/DavidMoraes-DEV/DMDeveloper/master/frontweb/src/assets/images/logos-technologies/tec-frontweb/logo-typescript.png", resume);
+		Skill skill15 = new Skill(null, "apex-charts", "https://raw.githubusercontent.com/DavidMoraes-DEV/ProjectsAssets/main/dmdeveloper/skills-logo/tec-frontweb/logo-apex-charts.png", resume);
+		Skill skill16 = new Skill(null, "axios", "https://raw.githubusercontent.com/DavidMoraes-DEV/ProjectsAssets/main/dmdeveloper/skills-logo/tec-frontweb/logo-axios.png", resume);
+		Skill skill17 = new Skill(null, "bootstrap", "https://raw.githubusercontent.com/DavidMoraes-DEV/ProjectsAssets/main/dmdeveloper/skills-logo/tec-frontweb/logo-bootstrap.png", resume);
+		Skill skill18 = new Skill(null, "css", "https://raw.githubusercontent.com/DavidMoraes-DEV/ProjectsAssets/main/dmdeveloper/skills-logo/tec-frontweb/logo-css.png", resume);
+		Skill skill19 = new Skill(null, "html", "https://raw.githubusercontent.com/DavidMoraes-DEV/ProjectsAssets/main/dmdeveloper/skills-logo/tec-frontweb/logo-html.png", resume);
+		Skill skill20 = new Skill(null, "jest", "https://raw.githubusercontent.com/DavidMoraes-DEV/ProjectsAssets/main/dmdeveloper/skills-logo/tec-frontweb/logo-jest.png", resume);
+		Skill skill21 = new Skill(null, "js", "https://raw.githubusercontent.com/DavidMoraes-DEV/ProjectsAssets/main/dmdeveloper/skills-logo/tec-frontweb/logo-js.png", resume);
+		Skill skill22 = new Skill(null, "netlify", "https://raw.githubusercontent.com/DavidMoraes-DEV/ProjectsAssets/main/dmdeveloper/skills-logo/tec-frontweb/logo-netlify.png", resume);
+		Skill skill23 = new Skill(null, "nodejs", "https://raw.githubusercontent.com/DavidMoraes-DEV/ProjectsAssets/main/dmdeveloper/skills-logo/tec-frontweb/logo-nodejs.png", resume);
+		Skill skill24 = new Skill(null, "react-hook-form", "https://raw.githubusercontent.com/DavidMoraes-DEV/ProjectsAssets/main/dmdeveloper/skills-logo/tec-frontweb/logo-react-hook-form.png", resume);
+		Skill skill25 = new Skill(null, "react-hook", "https://raw.githubusercontent.com/DavidMoraes-DEV/ProjectsAssets/main/dmdeveloper/skills-logo/tec-frontweb/logo-react-hook.png", resume);
+		Skill skill26 = new Skill(null, "react-router", "https://raw.githubusercontent.com/DavidMoraes-DEV/ProjectsAssets/main/dmdeveloper/skills-logo/tec-frontweb/logo-react-router.png", resume);
+		Skill skill27 = new Skill(null, "react", "https://raw.githubusercontent.com/DavidMoraes-DEV/ProjectsAssets/main/dmdeveloper/skills-logo/tec-frontweb/logo-react.png", resume);
+		Skill skill28 = new Skill(null, "typescript", "https://raw.githubusercontent.com/DavidMoraes-DEV/ProjectsAssets/main/dmdeveloper/skills-logo/tec-frontweb/logo-typescript.png", resume);
 		
 		categoryRepository.saveAll(Arrays.asList(catg1, catg2, catg3, catg4));
 		skillRepository.saveAll(Arrays.asList(skill1, skill2,skill3, skill4, skill5, skill6, skill7, skill8, skill9, skill10, skill11, skill12, skill13, skill14, skill15, skill16, skill17, skill18, skill19, skill20, skill21, skill22, skill23, skill24, skill25, skill26, skill27, skill28));
@@ -153,10 +153,10 @@ public class TestConfig implements CommandLineRunner {
 		
 		skillRepository.saveAll(Arrays.asList(skill1, skill2,skill3, skill4, skill5, skill6, skill7, skill8, skill9, skill10, skill11, skill12, skill13, skill14, skill15, skill16, skill17, skill18, skill19, skill20, skill21, skill22, skill23, skill24, skill25, skill26, skill27, skill28));
 		
-		Course course1 = new Course(null, "Algoritmo e Lógica de Programação do Básico ao Avançado", "Udemy - Geek University", 33.5, "https://raw.githubusercontent.com/DavidMoraes-DEV/DMDeveloper/master/frontweb/src/assets/files/certificado-algoritmo-logica-geek.pdf", resume);
-		Course course2 = new Course(null, "Algoritmo e Lógica de Programação(C, C++, C#, Java, Python", "Udemy - Nélio Alves", 30.5, "https://raw.githubusercontent.com/DavidMoraes-DEV/DMDeveloper/master/frontweb/src/assets/files/certificado-algoritmo-logica-nelio.pdf", resume);
-		Course course3 = new Course(null, "BootCamp TypeScript - React", "DEV Superior", 160.0, "https://raw.githubusercontent.com/DavidMoraes-DEV/DMDeveloper/master/frontweb/src/assets/files/certificado-bootcamp-react.pdf", resume);
-		Course course4 = new Course(null, "Bootcamp Java - Spring Boot", "DEV Superior", 160.0, "https://raw.githubusercontent.com/DavidMoraes-DEV/DMDeveloper/master/frontweb/src/assets/files/certificado-bootcamp-spring.pdf", resume);
+		Course course1 = new Course(null, "Algoritmo e Lógica de Programação do Básico ao Avançado", "Udemy - Geek University", 33.5, "https://github.com/DavidMoraes-DEV/ProjectsAssets/raw/main/files/certificado-algoritmo-logica-geek.pdf", resume);
+		Course course2 = new Course(null, "Algoritmo e Lógica de Programação(C, C++, C#, Java, Python", "Udemy - Nélio Alves", 30.5, "https://github.com/DavidMoraes-DEV/ProjectsAssets/raw/main/files/certificado-algoritmo-logica-nelio.pdf", resume);
+		Course course3 = new Course(null, "BootCamp TypeScript - React", "DEV Superior", 160.0, "https://github.com/DavidMoraes-DEV/ProjectsAssets/raw/main/files/certificado-bootcamp-react.pdf", resume);
+		Course course4 = new Course(null, "Bootcamp Java - Spring Boot", "DEV Superior", 160.0, "https://github.com/DavidMoraes-DEV/ProjectsAssets/raw/main/files/certificado-bootcamp-spring.pdf", resume);
 		Course course5 = new Course(null, "Java Completo - Programação Orientada a Objetos", "Udemy - Profº Nélio Alves", 55.0, "https://github.com/DavidMoraes-DEV/ProjectsAssets/raw/main/files/certificado-conclusao-java-completo-nelio.pdf", resume);
 		
 		courseRepository.saveAll(Arrays.asList(course1, course2, course3, course4, course5));
