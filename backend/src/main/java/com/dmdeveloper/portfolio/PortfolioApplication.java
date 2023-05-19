9120c7d1a89ae60aeebbbab9bd8f882fa9799066
DavidMoraes-DEV <investtraderrs@gmail.com>
2023-05-18 21:37:20 -0300

    Config Build railway

--- backend/src/main/java/com/dmdeveloper/portfolio/PortfolioApplication.java
+++ backend/src/main/java/com/dmdeveloper/portfolio/PortfolioApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class PortfolioApplication {
 
+	public String PORT = System.getenv("PORT");
+	
 	public static void main(String[] args) {
 		SpringApplication.run(PortfolioApplication.class, args);
 	}
